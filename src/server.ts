9b98811ebac5a9dd2f95e60f7cfc6d56515a4c98
Indiara Duarte <indiara96d@gmail.com>
2020-09-01 21:45:12 -0300

    Config eslint, prettier e editor config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express, { response } from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response)=> {
-    return response.json({message: 'Hello '});
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello ' });
 });
 
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333 =)');
+  console.log('Server started on port 3333 =)');
 });
