12317e238d03d3077e434981a88c985852a75412
Indiara Duarte <indiara96d@gmail.com>
2020-09-06 00:23:44 -0300

    Finalizing database connect

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,27 +2,34 @@ import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
 // Rota recebendo requisição
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
+
     //Tratamento de data
     const parsedDate = parseISO(date);
+
     //Instância do service
-    const createAppointment = new CreateAppointmentService(appointmentsRepository);
+    const createAppointment = new CreateAppointmentService();
+
     //Execução do service
-    const appointment = createAppointment.execute({ provider, date: parsedDate });
+    const appointment = await createAppointment.execute({ provider, date: parsedDate });
+
     //Retorno da execução do service
     return response.json(appointment);
   } catch (err) {
