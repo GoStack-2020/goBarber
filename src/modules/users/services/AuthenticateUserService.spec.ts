aeef0a8c37961772b0104ee16cbc0ccbdc6facf3
Indiara Duarte <indiara96d@gmail.com>
2020-10-13 18:59:13 -0300

    Finalizing user authenticate test

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -27,4 +27,35 @@ describe('AuthenticateUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
   });
+
+  it('Should not be able to create authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+    expect(authenticateUser.execute({
+      email: 'john@mail.com',
+      password: '123456'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to create authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'john@mail.com',
+      password: '123456'
+    });
+
+    expect(authenticateUser.execute({
+      email: 'john@mail.com',
+      password: 'wrong-password'
+    })).rejects.toBeInstanceOf(AppError);
+  });
 });
