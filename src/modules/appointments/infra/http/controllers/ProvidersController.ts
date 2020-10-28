ab8270db7038a4b1e379c2f2a580d12485b4c93a
Indiara Duarte <indiara96d@gmail.com>
2020-10-27 22:55:17 -0300

    Creating providers controller

---
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+
+export default class ProvidersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+
+    //Instância do service
+    const listProviders = container.resolve(ListProvidersService);
+
+    //Execução do service
+    const providers = await listProviders.execute({ user_id });
+
+    //Retorno da execução do service
+    return response.json(providers);
+  }
+}
