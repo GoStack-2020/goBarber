081f7064b1289d804d2b88379736c6080e9600e3
Indiara Duarte <indiara96d@gmail.com>
2020-10-29 21:59:29 -0300

    Updating appointments infra

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,20 +1,17 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 export default class AppointmentController {
   public async create(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
     const { provider_id, date } = request.body;
-
-    //Tratamento de data
-    const parsedDate = parseISO(date);
 
     //Instância do service
     const createAppointment = container.resolve(CreateAppointmentService);
 
     //Execução do service
-    const appointment = await createAppointment.execute({ provider_id, date: parsedDate });
+    const appointment = await createAppointment.execute({ provider_id, user_id, date });
 
     //Retorno da execução do service
     return response.json(appointment);
