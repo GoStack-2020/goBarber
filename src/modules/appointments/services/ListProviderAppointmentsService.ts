fb3866888ea2c0db1e3b0e8824c80138c00e4f64
Indiara Duarte <indiara96d@gmail.com>
2020-11-05 19:49:46 -0300

    Updating list provider with cache methods

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface Request {
   provider_id: string;
@@ -14,15 +15,22 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({ provider_id, day, year, month }: Request): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('asd');
+
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
       provider_id,
       day,
       month,
       year,
     });
+
+    // await this.cacheProvider.save('as', 'as');
 
     return appointments;
   }
